b706273d01fcf2efd42f6a44a12ab8528b63a088
juanc <juancruzberrios73@gmail.com>
2026-04-09 22:37:35 -0300

    aasdw

--- patrones/creacionales/TorreControl.java
+++ patrones/creacionales/TorreControl.java
@@ -1,19 +1,14 @@
 package patrones.creacionales;
 
-/**
- * Ejemplo de Patrón Creacional: Singleton
- * Representa la Torre de Control de un Aeropuerto. Solo puede haber una.
- */
+
 public class TorreControl {
     private static TorreControl instancia;
     private String nombre;
 
-    // Constructor privado para evitar instanciación externa
     private TorreControl() {
         this.nombre = "Torre de Control Central - Aconcagua";
     }
 
-    // Método estático para obtener la única instancia
     public static synchronized TorreControl getInstancia() {
         if (instancia == null) {
             instancia = new TorreControl();
@@ -31,7 +26,6 @@ public class TorreControl {
 
         torre1.enviarInstrucciones("AR1234", "Pista 1");
 
-        // Comprobación de que es la misma instancia
         if (torre1 == torre2) {
             System.out.println("Ambas variables referencian a la misma instancia de la Torre de Control.");
         }
